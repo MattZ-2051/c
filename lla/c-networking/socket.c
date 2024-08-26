4c054adbd82185742869542c30124313baf2a085
Matt Zamora <mzamora2899@gmail.com>
2024-08-26 07:34:44 -0600

    mover lla files into lla folder

---
+++ lla/c-networking/socket.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+int main() {
+  int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (fd == -1) {
+      perror("socket");
+      return -1;
+  }
+
+  printf("%d\n", fd);
+
+
+}
