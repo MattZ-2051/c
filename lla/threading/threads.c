0693d7dcc71a863693fb6a6a83b9813af658f44e
Matt Zamora <mzamora2899@gmail.com>
2024-09-09 14:39:51 -0600

    more notes on c threads

--- lla/threading/threads.c
+++ lla/threading/threads.c
@@ -4,17 +4,32 @@
 
 #define THREAD_COUNT 10
 
-void *thread_target() { printf("I am a thread\n"); }
+typedef struct {
+  int arg1;
+  short arg2;
+} thread_arg_t;
+
+void *thread_target(void *vargs) {
+  thread_arg_t *args = (thread_arg_t *)vargs;
+  printf("I am a thread, and %d\n", args->arg1);
+}
 
 int main(int argc, char *argv[]) {
 
   pthread_t threads[THREAD_COUNT];
+
+  thread_arg_t myargs;
+
   int i = 0;
   for (i = 0; i < THREAD_COUNT; i++) {
-    if (pthread_create(&threads[i], NULL, thread_target, NULL)) {
+    myargs.arg1 = i;
+    if (pthread_create(&threads[i], NULL, thread_target, (void *)&myargs)) {
       return -1;
     }
   }
 
+  for (i = 0; i < THREAD_COUNT; i++) {
+    pthread_join(threads[i], NULL);
+  }
   return 0;
 }
